c13fb1d8a4e1b868a44100a8a35b3b71d7a8f447
Mimimi-d <mimimi.dev.1202@gmail.com>
2022-06-02 02:00:30 +0900

    create timer page

--- lib/ui/timer/timer_page.dart
+++ lib/ui/timer/timer_page.dart
@@ -9,29 +9,205 @@ import 'package:my_first_app/ui/calendar/calendar_card.dart';
 import '../login/login_page.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../../widgets/round_button.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 
-class TimerPage extends StatelessWidget {
+// class TimerPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Builder(builder: (context) {
+//       return Scaffold(
+//           appBar: AppBar(
+//             title: Text('タイマー'),
+//             actions: <Widget>[
+//               IconButton(
+//                 icon: Icon(Icons.close),
+//                 onPressed: () async {
+//                   await FirebaseAuth.instance.signOut();
+//                   await Navigator.of(context).pushReplacement(
+//                     MaterialPageRoute(builder: (context) {
+//                       return LoginPage();
+//                     }),
+//                   );
+//                 },
+//               ),
+//             ],
+//           ),
+//           body: Center(child: Text('aaa')));
+//     });
+//   }
+// }
+
+class TimerPage extends StatefulWidget {
+  const TimerPage({Key? key}) : super(key: key);
+
+  @override
+  _CountdownPageState createState() => _CountdownPageState();
+}
+
+class _CountdownPageState extends State<TimerPage>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  bool isPlaying = false;
+
+  String get countText {
+    Duration count = controller.duration! * controller.value;
+    return controller.isDismissed
+        ? '${controller.duration!.inHours}:${(controller.duration!.inMinutes % 60).toString().padLeft(2, '0')}:${(controller.duration!.inSeconds % 60).toString().padLeft(2, '0')}'
+        : '${count.inHours}:${(count.inMinutes % 60).toString().padLeft(2, '0')}:${(count.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  double progress = 0.001;
+
+  void notify() {
+    if (countText == '0:00:00') {
+      FlutterRingtonePlayer.playNotification();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 6),
+    );
+
+    controller.addListener(() {
+      notify();
+      if (controller.isAnimating) {
+        setState(() {
+          progress = controller.value;
+        });
+      } else {
+        setState(() {
+          progress = 1.0;
+          isPlaying = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Builder(builder: (context) {
-      return Scaffold(
-          appBar: AppBar(
-            title: Text('タイマー'),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.close),
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut();
-                  await Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) {
-                      return LoginPage();
-                    }),
-                  );
-                },
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('タイマー'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              await Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) {
+                  return LoginPage();
+                }),
+              );
+            },
           ),
-          body: Center(child: Text('aaa')));
-    });
+        ],
+      ),
+      backgroundColor: Color(0xfff5fbff),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  width: 300,
+                  height: 300,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.grey.shade300,
+                    value: progress,
+                    strokeWidth: 20,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isDismissed) {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => Container(
+                          height: 300,
+                          child: CupertinoTimerPicker(
+                            initialTimerDuration: controller.duration!,
+                            onTimerDurationChanged: (time) {
+                              setState(() {
+                                controller.duration = time;
+                              });
+                            },
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: Center(
+                    child: AnimatedBuilder(
+                      animation: controller,
+                      builder: (context, child) => Text(
+                        countText,
+                        style: const TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "monospace",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isAnimating) {
+                      controller.stop();
+                      setState(() {
+                        isPlaying = false;
+                      });
+                    } else {
+                      controller.reverse(
+                          from: controller.value == 0 ? 1.0 : controller.value);
+                      setState(() {
+                        isPlaying = true;
+                      });
+                    }
+                  },
+                  child: RoundButton(
+                    icon: isPlaying == true ? Icons.pause : Icons.play_arrow,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    controller.reset();
+                    setState(() {
+                      isPlaying = false;
+                    });
+                  },
+                  child: RoundButton(
+                    icon: Icons.stop,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
